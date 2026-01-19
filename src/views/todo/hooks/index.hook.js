c1c5cac618fa97d235f6df6f8259ee716393baea
nonlaphan <nonlaphan.pim@gmail.com>
2026-01-19 23:35:39 +0700

    refac: refactor index.hook

--- src/views/todo/hooks/index.hook.js
+++ src/views/todo/hooks/index.hook.js
@@ -1,24 +1,30 @@
 import { useState, useEffect } from "react";
 import { useDebounce } from "@/hooks/useDebounce";
-import {  useGetPageList, useGetTaskByPage, useUpdatePageTitle } from "@/queries/pages.queries";
+import {
+  useGetPageList,
+  useGetTaskByPage,
+  useUpdatePageTitle,
+} from "@/queries/pages.queries";
 import { useEditTask } from "@/queries/tasks.queries";
 import { useGetConnectionByPage } from "@/queries/connections.queries";
+import { usePageStore } from "@/stores/usePageStore";
 
-export function useIndexKanbanBoard(userId) {
+export function useIndexKanbanBoard() {
   const [board, setBoard] = useState({});
   const [editingTask, setEditingTask] = useState(null);
   const [title, setTitle] = useState("");
-  const [connections, setConnections] = useState({});
 
-  const editTaskMutation = useEditTask(userId);
-  const { data: pageList } = useGetPageList(userId);
-  const pageId = pageList?.items?.[0]?.id;
-
+  const { setPageId } = usePageStore();
   const debouncedTitle = useDebounce(title, 3000);
+  
+  // -- query api --
+  const { data: pageList } = useGetPageList();
+  const pageId = pageList?.items?.[0]?.id;
+  
+  const editTaskMutation = useEditTask(pageId);
   const updateTitle = useUpdatePageTitle(pageId);
-
+  
   const { data: taskData, isLoading: isTaskLoading } = useGetTaskByPage(pageId);
-
   const { data: connectionData } = useGetConnectionByPage(pageId);
 
   // init title when page load
@@ -28,24 +34,23 @@ export function useIndexKanbanBoard(userId) {
     }
   }, [pageList]);
 
+  // load tasks into board
+  useEffect(() => {
+    if (taskData) {
+      setBoard(taskData?.tasks);
+    }
+  }, [taskData]);
+
+  // set id to store
+  useEffect(() => {
+    if (pageId) setPageId(pageId);
+  }, [pageId]);
+
   // autosave title of page
   useEffect(() => {
     if (!debouncedTitle || !pageId) return;
     updateTitle.mutate(debouncedTitle);
   }, [debouncedTitle, pageId]);
-
-  // load tasks into board
-  useEffect(() => {
-    if (taskData) {
-      setBoard(taskData.tasks);
-    }
-  }, [taskData]);
-
-  useEffect(() => {
-    if (connectionData) {
-      setConnections(connectionData);
-    }
-  }, [connectionData]);
 
   return {
     board,
@@ -57,6 +62,6 @@ export function useIndexKanbanBoard(userId) {
     setTitle,
     editTaskMutation,
     isTaskLoading,
-    connections,
+    connections: connectionData,
   };
 }
